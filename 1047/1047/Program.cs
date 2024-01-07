05aa16ea723444a696a760f2a84635a58caf4d76
domingos cadete <115948082+Domingoscadete@users.noreply.github.com>
2024-01-07 22:30:11 +0100

    Add files via upload

---
+++ 1047/1047/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1047
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] n1 = Console.ReadLine().Split(' ');
+            int n2, n3, n4, n5,s1,s2,tempo,hr,min;
+            n2 = int.Parse(n1[0]);
+            n3 = int.Parse(n1[1]);
+            n4 = int.Parse(n1[2]);
+            n5 = int.Parse(n1[3]);
+            if (n4 <= n2 && n5 <= n3)
+            {
+                n4 = n4 + 24;
+
+            }
+            else if (n5 <=n3)
+            {
+                n5 = n5 + 60;
+                n4 = n4 - 1;
+            }
+
+            s1 = (n2 * 3600) + (n3 * 60);
+            s2 = (n4 * 3600) + (n5 * 60);
+
+            tempo = (s2 - s1);
+            hr = tempo / 3600;
+            min = (tempo - (hr * 3600)) / 60;
+
+            Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", hr, min);
+        }
+    }
+}
---
+++ 1048/1048/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1048
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] n1 = Console.ReadLine().Split(' ');
+            double n2,n3;
+            n2 = double.Parse(n1[0]);
+            if (n2 <= 400.00)
+            {
+                n3 = (n2 * 15) / 100;
+                Console.WriteLine($"Novo salario: {(n2+n3).ToString("F2")}");
+                Console.WriteLine($"Reajuste ganho: {(n3).ToString("F2")}");
+                Console.WriteLine("Em percentual: 15 %");
+            }
+            else  if(n2>= 400.01 && n2<=800.00)
+            {
+                n3 = (n2 * 12) / 100;
+                Console.WriteLine($"Novo salario: {(n2 + n3).ToString("F2")}");
+                Console.WriteLine($"Reajuste ganho: {(n3).ToString("F2")}");
+                Console.WriteLine("Em percentual: 12 %");
+            }
+            else if(n2 >= 800.01 && n2 <= 1200.00)
+            {
+                n3 = (n2 * 10) / 100;
+                Console.WriteLine($"Novo salario: {(n2 + n3).ToString("F2")}");
+                Console.WriteLine($"Reajuste ganho: {(n3).ToString("F2")}");
+                Console.WriteLine("Em percentual: 10 %");
+            }
+            else if(n2 >= 1200.01 && n2 <= 2000.00)
+            {
+                n3 = (n2 * 7) / 100;
+                Console.WriteLine($"Novo salario: {(n2 + n3).ToString("F2")}");
+                Console.WriteLine($"Reajuste ganho: {(n3).ToString("F2")}");
+                Console.WriteLine("Em percentual: 7 %");
+            }
+            else if(n2 > 2000.0)
+            {
+                n3 = (n2 * 4) / 100;
+                Console.WriteLine($"Novo salario: {(n2 + n3).ToString("F2")}");
+                Console.WriteLine($"Reajuste ganho: {(n3).ToString("F2")}");
+                Console.WriteLine("Em percentual: 4 %");
+
+            }
+        }
+    }
+}
